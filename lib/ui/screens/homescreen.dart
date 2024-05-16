958b4567327ce38cbbeb7792c99d7da9da05df25
Felizberto de Miranda <felizberto11fred@gmail.com>
2024-05-16 01:29:19 -0300

    update: new widget added

--- lib/ui/screens/homescreen.dart
+++ lib/ui/screens/homescreen.dart
@@ -1,8 +1,9 @@
-import 'package:fintech_mobile_app/ui/components/button_icon.dart';
+import 'package:fintech_mobile_app/ui/components/icon_button.dart';
+import 'package:fintech_mobile_app/ui/components/icon_button_outlined.dart';
 import 'package:fintech_mobile_app/ui/components/send_money_icon.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import '../constants/button_icon_background_color.dart';
+import '../constants/button_icon_color.dart';
+import '../constants/button_icon_dimensions.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,7 +15,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
+    // double screenWidth = MediaQuery.of(context).size.width;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -39,7 +40,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     CustomIconButton(
                       icon: const Icon(Icons.wallet),
                       backgroundColor: customButtonBackgroundColor,
-                      minimumSize: const Size(95, 60),
+                      minimumSize: const Size(95, buttonIconHeight),
                       onPressed: () {},
                     ),
                     const SizedBox(
@@ -48,7 +49,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     CustomIconButton(
                       icon: const Icon(Icons.currency_exchange_outlined),
                       backgroundColor: customButtonBackgroundColor,
-                      minimumSize: const Size(95, 60),
+                      minimumSize: const Size(95, buttonIconHeight),
                       onPressed: () {},
                     ),
                   ],
@@ -98,38 +99,140 @@ class _HomeScreenState extends State<HomeScreen> {
                     borderRadius: BorderRadius.circular(25),
                     color: customButtonBackgroundColor,
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 60),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CircleAvatar(
+                                radius: 35,
+                                backgroundImage:
+                                    AssetImage('assets/img_logo/fbc.jpg'),
+                              ),
+                              Text(
+                                'Federal Government',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                              CustomIconButtonOutlined(
+                                icon: Icons.edit_square,
+                                backgroundColor: customButtonBackgroundColor,
+                                minimumSize: Size(65, 65),
+                              )
+                            ],
+                          ),
+                        ),
+                        Text(
+                          'Montly Payment',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
+                        ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            const CircleAvatar(
-                              radius: 35,
-                              backgroundImage:
-                                  AssetImage('assets/img_profiles/fbc.jpg'),
+                            Stack(
+                              alignment: Alignment.topCenter,
+                              children: [
+                                Text(
+                                  '\$26.999',
+                                  style: TextStyle(
+                                      fontSize: 35, // Tamanho da fonte
+                                      color: Colors.black,
+                                      fontWeight:
+                                          FontWeight.bold // Cor do texto
+                                      ),
+                                ),
+                                Positioned(
+                                  top: 6,
+                                  left: 135,
+                                  child: Text(
+                                    ',99',
+                                    style: TextStyle(
+                                      fontSize: 16, // Tamanho da fonte
+                                      color: Colors.grey, // Cor do texto
+                                      fontStyle: FontStyle.italic, // Itálico
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
-                            const Text(
-                              'Federal Government',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
+                            CustomIconButtonOutlined(
+                              icon: Icons.arrow_forward,
+                              backgroundColor: customButtonBackgroundColor,
+                              minimumSize: Size(105, buttonIconHeight),
+                              shape: 40,
+                              leftText: Text(
+                                'All',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 18),
                               ),
                             ),
-                            IconButton.outlined(
-                              icon: const Icon(Icons.edit_square),
-                              style: ButtonStyle(
-                                iconColor: WidgetStateProperty.all<Color>(Colors.black),
-                                minimumSize: WidgetStateProperty.all<Size>(const Size(65, 65))
-                              ),
-                              onPressed: () {},
-                            )
                           ],
                         ),
                       ],
                     ),
                   ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20, bottom: 50),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const CustomIconButtonOutlined(
+                        backgroundColor: customButtonBackgroundColor,
+                        leftText: Text(
+                          'Test',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                        minimumSize: Size(170, buttonIconHeight),
+                      ),
+                      CustomIconButtonOutlined(
+                        backgroundColor: const Color.fromRGBO(217, 255, 126, 1),
+                        leftText: const Text(
+                          'Next',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                        minimumSize: const Size(170, buttonIconHeight),
+                        onClick: () {},
+                      ),
+                    ],
+                  ),
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      'Privacy Policy',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      ),
+                    ),
+                    Text(
+                      'Terms of Use',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
